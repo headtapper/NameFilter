b2c65c014aa51fc5ec73b0de33651b4b53c925b5
headtapper <rustcontent@outlook.com>
2025-09-13 18:12:41 -0300

    Create NameFilter.cs

---
+++ NameFilter.cs
@@ -0,0 +1,47 @@
+using System;
+using Oxide.Core.Plugins;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Oxide.Plugins
+{
+    [Info("NameFilter", "headtapper", "1.0.0")]
+    [Description("Allow connections from players with usernames containing only English alhpanumeric characters. ")]
+
+    public class NameFilter : RustPlugin
+    {
+        #region Initialization
+
+        private const string NameFilterBypassPermission = "namefilter.bypass";
+
+        private void Init()
+        {
+            permission.RegisterPermission(NameFilterBypassPermission, this);
+        }
+
+        #endregion
+
+        #region Localization
+
+        protected override void LoadDefaultMessages()
+        {
+            lang.RegisterMessages(new Dictionary<string, string>
+            {
+                ["KickMessage"] = "Your name must only contain English alphanumeric characters.",
+            }, this);
+        }
+
+        #endregion
+
+        #region Hooks
+
+        private void OnPlayerConnected(BasePlayer player)
+        {
+            if (!player.displayName.All(char.IsLetterOrDigit) && !permission.UserHasPermission(player.UserIDString, NameFilterBypassPermission))
+                player.Kick(lang.GetMessage("KickMessage", this, player.UserIDString));
+        }
+
+        # endregion
+    }
+}
+
